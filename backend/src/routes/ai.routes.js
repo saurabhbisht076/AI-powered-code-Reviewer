84392cd80223d051bc425e864a6f9d5faaabdfed
Saurabh <saurabhbisht076@gmail.com>
2025-02-24 02:28:49 +0530

    Updated backend/src/routes/ai.routes.js

--- backend/src/routes/ai.routes.js
+++ backend/src/routes/ai.routes.js
@@ -2,5 +2,5 @@ const express = require('express');
 const aiController = require('../controllers/ai.controller');
 const router = express.Router();
 
-router.get("/get-response",aiController.getResponse);
+router.get("/get-review",aiController.getReview);
 module.exports = router;
