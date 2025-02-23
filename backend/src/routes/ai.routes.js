e7a73e6dbacd894bf835dd724c9b872af821ec55
Saurabh <saurabhbisht076@gmail.com>
2025-02-24 01:42:03 +0530

    Add backend/src/routes/ai.routes.js

---
+++ backend/src/routes/ai.routes.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const aiController = require('../controllers/ai.controller');
+const router = express.Router();
+
+router.get("/get-response",aiController.getResponse);
+module.exports = router;
